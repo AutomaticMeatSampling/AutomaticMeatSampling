f59b3b498fc532598a144d5a06b40e8747b86262
sanch504 <sanch504@purdue.edu>
2025-10-21 12:48:14 -0400

    Update new_test.py

--- Robot_API_Tests/new_test.py
+++ Robot_API_Tests/new_test.py
@@ -21,10 +21,6 @@ def main():
 
     photograph_offset = 40 #y offset (was -40)
 
-    
-
-
-
     #########################
 
     grid_pixels_x = 55
@@ -37,12 +33,12 @@ def main():
     y_mm_per_pixel = grid_mm/grid_pixel_y
 
     ## INPUT HERE
-    dot_x_pixel = 1610;#test value
-    dot_y_pixel = 252;#test value
+    dot_x_pixel = 878;#test value  1774, 1862, 1494, 810, 878
+    dot_y_pixel = 482;#test value   842, 584,   530, 758, 482
     ## INPUT HERE
 
-    pipette_offset_x = 0*x_mm_per_pixel
-    pipette_offset_y = 606*y_mm_per_pixel
+    pipette_offset_x = -5*x_mm_per_pixel
+    pipette_offset_y = 609*y_mm_per_pixel #606
 
     pixel_move_x_mm = ((dot_x_pixel - x_camera_center_offset_pixel)*x_mm_per_pixel) + pipette_offset_x;
     pixel_move_y_mm = -((dot_y_pixel - y_camera_center_offset_pixel)*y_mm_per_pixel) - pipette_offset_y;
@@ -55,12 +51,19 @@ def main():
     # Step 1: At initiation, always go home first
 
     dexarm.go_home()
-    dexarm.move_to(0, y_home+photograph_offset, 150)#photographing position
+    #dexarm.move_to(0, y_home+photograph_offset, 150)#photographing position
 
     #note: maximum to move downwards like that is y = -70, z = -80
 
+    # Step 2: Movement
     dexarm.fast_move_to(0, y_home+photograph_offset, 0)
-    dexarm.move_inward_to_target(pixel_move_x_mm, y_home + photograph_offset + pixel_move_y_mm, -80, rotation_mode) #need to check if value is left or right to say clockwise or counterclockwise
+    dexarm.move_inward_to_target(pixel_move_x_mm, y_home + photograph_offset + pixel_move_y_mm, -80, rotation_mode) 
+
+    time.sleep(3)
+    x_curr, y_curr, z_curr, *_ = dexarm.get_current_position()
+    print(f" actual X: {x_curr}") 
+    print(f" actual Y: {y_curr}") 
+    print(f" actual Z : {z_curr}")
 
     # Final Step: Close the serial port
     dexarm.close()
