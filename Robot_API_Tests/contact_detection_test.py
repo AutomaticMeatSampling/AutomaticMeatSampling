67a0454de23a21db6123e9f742d4b396bf21efc5
Ryan Jacob <rmadhuja@purdue.edu>
2025-10-23 16:41:17 -0400

    Added test script for pipette height detection

---
+++ Robot_API_Tests/contact_detection_test.py
@@ -0,0 +1,86 @@
+from pydexarm import Dexarm
+import serial
+import serial.tools.list_ports
+import time
+import re
+
+
+def id_serial_ports():
+    # Identify COM ports for microcontroller and Rotrics arm
+    returned_txt = ''
+    msg_list = ['R', 'M']
+    ser_micro_find = None
+    dexarm = None
+    ports = serial.tools.list_ports.comports()
+    print('Searching for COM ports')
+
+    for port in ports:
+        found_port = False
+        for msg in msg_list:
+            try:
+                ser = serial.Serial(port=port.device, baudrate=115200, timeout=0.1)
+                time.sleep(1)
+                ser.flushInput()
+                ser.write(bytes(msg, 'utf-8'))
+                returned_txt = ser.readline()
+                ser.flushInput()
+
+                if returned_txt == b'R here \n':
+                    ser_micro_find = ser
+                    print(f'Found microcontroller at {port.device}')
+                    found_port = True
+                    ser.close()
+                elif returned_txt == 'ok':
+                    dexarm = Dexarm(port=port.device)
+                    print(f'Found Rotrics Dexarm at {port.device}')
+                    found_port = True
+                else:
+                    ser.close()
+            except serial.SerialException as e:
+                print(f'Error opening serial port: {e}')
+
+            except Exception as e:
+                print(f'Unexpected error during serial port initialization: {e}')
+
+            if found_port:
+                break
+    return ser_micro_find, dexarm
+
+
+def ser_close(ser_micro, dexarm):
+    ser_list = [ser_micro, dexarm]
+    for ser in ser_list:
+        try:
+            ser.close()
+        except AttributeError as e:
+            print(f'Error closing port: {e}')
+
+
+def move_down(ser_micro, dexarm):
+    connect = True
+    received_msg = ''
+    ser_micro.flushInput()
+    ser_micro.write(bytes('D', 'utf-8'))
+    move_step = -0.2
+    print('Checking Connection')
+    z_pos = 0
+    while connect:
+        received_msg = ser_micro.readline()
+        ser_micro.flushInput()
+        dexarm.move_to(x=None, y=None, z=z_pos, e=None, feedrate=2000, mode='G1', wait=True)
+        z_pos = z_pos - move_step
+        if received_msg == b'DISCONNECTED\r\n':
+            connect = False
+            print('Disconnected')
+
+    # Maybe need to pause for 5 seconds over here
+
+
+# Main
+if __name__ == '__main__':
+    ser_micro, dexarm = id_serial_ports()
+    ser_micro.open()
+
+    move_down(ser_micro, dexarm)  # move dexarm down until microcontroller signals broken contact
+
+    ser_close(ser_micro, dexarm)
