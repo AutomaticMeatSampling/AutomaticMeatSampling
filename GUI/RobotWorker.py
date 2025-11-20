122e854c8d24523041ef92ada77860fea9d15010
Emily Loiselle <emilyannaloiselle@gmail.com>
2025-11-19 19:19:01 -0500

    Update gui to incoporate robot movement actually woooh

--- GUI/RobotWorker.py
+++ GUI/RobotWorker.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import serial
 import time
 from ImageWorker import ImageWorker
+import cv2
 
 repo_root = Path(__file__).resolve().parent.parent  # Repo/
 seg_folder = repo_root / "Robot_API_Tests"
@@ -14,61 +15,154 @@ from takepicture import take_photo
 
 class RobotWorker(QThread):
     progress = pyqtSignal(str)
-    steps = ["PHOTO", "TIP_PICKUP", "SOLVENT_PICKUP", "SAMPLE_COLLECTION", "SAMPLE_DROPOFF", "TIP_DISPOSAL"]
+    steps = ["TIP_PICKUP", "SOLVENT_PICKUP", "MOVE_TO_XY_SAMPLE_COORD", "MOVE_DOWN_TO_SAMPLE_COORD", "SAMPLE_DROPOFF", "TIP_DISPOSAL"]
 
-    def __init__(self, sample_mode, main_window):
+    def __init__(self, sample_mode, main_window, curr_tip=1, max_tip_num=6, curr_vial=1, max_vial_num=3, use_robot_simulator=False, use_simulated_sample=True):
         super().__init__()
+
+        # ------------------------
+        # Simulator configurations
+        # ------------------------
+        self.use_robot_simulator = use_robot_simulator
+        self.use_simulated_sample = use_simulated_sample
+
+        #--------------------------------
+        # Sample Collection Configuration
+        #--------------------------------
+        self.sample_mode = sample_mode
+        self.total_num_samples = 1 # Assume 1 sample at start, will be updated with image processing results
+
+        #--------------------------------------
+        # Track of current pipette tip and vial
+        #--------------------------------------
+        self.curr_tip = curr_tip
+        self.max_tip_num = max_tip_num
+        self.curr_vial = curr_vial
+        self.max_vial_num = max_vial_num
+
+        #---------------
+        # Status/Flags
+        #---------------
         self.complete_success = False
         self._stop_requested = False
-        self.sample_mode = sample_mode
+        self.image_processing_complete = False
+        self.last_completed_step = None
+        
         self.main_window = main_window
 
-        # Setup connction to robot here
+        #--------------------------------
+        # Setup connection to robot ports
+        #--------------------------------
         print("Setting up robot connections...")
-        # self.dexarm = Dexarm(port="COM3")
-        # self.ser_micro = serial.Serial(port='COM4', baudrate=115200, timeout=0.1)
+        if not self.use_robot_simulator:
+            self.dexarm = Dexarm(port="COM3")
+            self.ser_micro = serial.Serial(port='COM4', baudrate=115200, timeout=0.1)
 
     def run(self):
         self.progress.emit("ROBOT_START")
         print(f"Running robot worker process with sample mode: {self.sample_mode}")
 
-        for step in RobotWorker.steps:
-            if self._stop_requested:
+        # --------------------------------------------------------------------------------
+        # STEP 0: Initial step of getting photo, sets off automatic/manual coord selection
+        # --------------------------------------------------------------------------------
+        self.collect_photo()
+        self.last_completed_step = "PHOTO"
+
+        # Start sample process for each sample pt...
+        num_samples_collected = 0
+        while num_samples_collected < self.total_num_samples:
+            num_samples_collected += 1
+            self.progress.emit(f"Starting robot movement for sample {num_samples_collected}")
+
+            # ------------------------------------
+            # Error Checking: Check tip/vial count
+            # ------------------------------------
+            if self.isOverMaxTipNum() or self.isOverMaxVialNum():
+                self.progress.emit("Not enough tips or vials to complete remaining samples")
                 self.finish()
-                return
-            
-            self.progress.emit(step)
 
-            match step:
-                case "PHOTO":
-                    self.collect_photo()
-                case "TIP_PICKUP":
-                    time.sleep(2)
-                case "SOLVENT_PICKUP":
-                    time.sleep(2)
-                case "SAMPLE_COLLECTION":
-                    if self.sample_mode == "Automatic":
-                        timeout = 10
-                    else:
-                        timeout = 100
-                    if not self.main_window.image_coords_ready_event.wait(timeout=timeout):
-                        self.progress.emit("Error: Image coord result not received in time")
-                        self.finish()
-                        return
-                    
-                    muscle_pts = self.main_window.selected_points["muscle_points"]
-                    marbling_pts = self.main_window.selected_points["marbling_points"]
-                    self.progress.emit("Received coordinates!!!")
+            # -------------------------------------------------------
+            # Loop through sample collection steps for each sample pt
+            # -------------------------------------------------------
+            for step in RobotWorker.steps:
 
-                    # Clear even for next use
-                    self.main_window.image_coords_ready_event.clear()
+                # ----------------------------------
+                # Watch for STOP interrupt between steps
+                # ----------------------------------
+                if self._stop_requested:
+                    self.finish()
+                    return
+                
+                self.progress.emit(step)
+
+                # ------------------------------------------
+                # SIMULATED ROBOT MOVEMENT
+                # ------------------------------------------
+                if self.use_robot_simulator:
                     time.sleep(2)
-                case "SAMPLE_DROPOFF":
-                    time.sleep(2)
-                case "TIP_DISPOSAL":
-                    time.sleep(2)
-                case _:
-                    print("Unknown step what???")
+                    break
+                
+
+                match step:
+
+                    # ------------------------------------------------------------------
+                    # STEP 1: (a) Move to pipette tip area and (b) Pick up a pipette tip
+                    # ------------------------------------------------------------------
+                    case "TIP_PICKUP":
+                        self.dexarm.move_to_pipette_tip(self.curr_tip)
+                        self.curr_tip += 1
+
+                    # --------------------------------------------------------
+                    # STEP 2: (a) Move to solvent tube and (b) Collect solvent
+                    # --------------------------------------------------------
+                    case "SOLVENT_PICKUP":
+                        self.dexarm.step_3_move_to_solvent()
+
+                    # ---------------------------------------------------------------
+                    # STEP 3: (a) Calculate robot coordinate, may need to wait for coords from main_window (auto/manual) & (b) move to calculated location
+                    # ---------------------------------------------------------------
+                    case "MOVE_TO_XY_SAMPLE_COORD":
+
+                        # If first time here, may need to wait for results to appear
+                        if not self.image_processing_complete:
+                            ret = self.wait_for_coord_results()
+                            if ret == -1:
+                                self.finish()
+                                return
+                            
+                        # ********* TODO: ADD FUNC TO MOVE TO sample XY location (Breanna) ***************
+                        self.dexarm.move_to(0, 187, 45) # Temporary test location
+                            
+                    # ----------------------------------------
+                    # STEP 4: Move downward to sample and hold
+                    # ----------------------------------------
+                    case "MOVE_DOWN_TO_SAMPLE_COORD":
+                        self.dexarm.move_down_meat(self.ser_micro)
+
+                    # ------------------------------------------------
+                    # STEP 5: (a) Move to vial and (b) Dispense liquid
+                    # ------------------------------------------------
+                    case "SAMPLE_DROPOFF":
+                        self.dexarm.step_5_dispense_sample(self.curr_vial)
+                        self.curr_vial += 1
+
+                    # ------------------------------------------------------------
+                    # STEP 6: (a) Move to dispose cup and (b) Drop off pipette tip
+                    # ------------------------------------------------------------
+                    case "TIP_DISPOSAL":
+                        self.dexarm.step_6_move_to_dispose_cup()
+
+                    # UNKNOWN case, should never happen
+                    case _:
+                        print("UNKNOWN STEP")
+                
+                # -------------------------------------
+                # UPDATE STATUS of last completed step
+                # -------------------------------------
+                self.last_completed_step = step
+
+            # After collecting sample, go home
+            self.dexarm.go_home()
 
         self.complete_success = True
         self.finish()
@@ -77,8 +171,13 @@ class RobotWorker(QThread):
         if self.complete_success:
             self.progress.emit("ROBOT_STOP_SUCCESS")
         else:
+            # If not completed successuflly need to do some clean up steps
             self.progress.emit("ROBOT_STOP")
         self._stop_requested = False
+
+        if not self.use_robot_simulator:
+            self.dexarm.close()
+        self.ser_micro.close()
 
     
     def stop(self):
@@ -87,14 +186,63 @@ class RobotWorker(QThread):
             self._stop_requested = True
 
     def collect_photo(self):
-        # Do steps to move to photo position
+        """
+        Collect photo and start coordinate selection from main thread (automatic or manual)
+        """
+        # Move to photo position:
+        if not self.use_robot_simulator:
+            photograph_offset_y = 30
+            photograph_offset_z = 110
+            self.dexarm.go_home()
+            self.dexarm.move_to(0, self.dexarm.y_home+photograph_offset_y, photograph_offset_z)
+            # TODO might need to wait a bit before img is taken - to stabilize
+        else:
+            # Imitate robot movement to photo position
+            time.sleep(2)
 
         # Take photo
-        # take_photo(ImageWorker.img_path)
+        if self.use_simulated_sample:
+            simulated_img = cv2.imread("simulated_img.png")
+            cv2.imwrite(ImageWorker.img_path, simulated_img)
+        else:
+            take_photo(ImageWorker.img_path)
 
         if self.sample_mode == "Manual":
             self.progress.emit("START_MANUAL_POINT_SELECTION")
         elif self.sample_mode == "Automatic":
             self.progress.emit("START_AUTOMATIC_POINT_SELECTION")
 
-        time.sleep(2)
+    def wait_for_coord_results(self):
+        """ Wait for results for specific timeout amount """
+
+        timeout = 10 if self.sample_mode == "Automatic" else 100
+        if not self.main_window.image_coords_ready_event.wait(timeout=timeout):
+            self.progress.emit("Error: Image coord result not received in time")
+            return -1
+        
+        # Clear event
+        self.main_window.image_coords_ready_event.clear()
+        
+        # Update progress/status
+        self.progress.emit("Robot Worker Received Coordinates!!!")
+        self.image_processing_complete = True
+
+        # ----------------------------
+        # Parse selected sample points
+        # ----------------------------
+        self.muscle_pts = self.main_window.selected_points["muscle_points"]
+        self.marbling_pts = self.main_window.selected_points["marbling_points"]
+        self.total_num_samples = len(self.muscle_pts) + len(self.marbling_pts)
+
+        # Check that total number of points is greater than 0 / Additional check of STOP interrupt
+        if self.total_num_samples < 1 or self._stop_requested:
+            self.progress.emit("Error: total number of samples is less than 1") if self.total_num_samples < 1 else self.progress.emit("STOP_INTERRUPT")
+            return -1
+
+    def isOverMaxTipNum(self):
+        """ Check current tip within bounds """
+        return self.curr_tip > self.max_tip_num
+    
+    def isOverMaxVialNum(self):
+        """ Check current vial within bounds """
+        return self.curr_vial > self.max_vial_num
--- GUI/gui.py
+++ GUI/gui.py
@@ -3,11 +3,13 @@ import time
 from ImageWorker import ImageWorker
 from RobotWorker import RobotWorker
 from threading import Event
+from datetime import datetime
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton,
-    QVBoxLayout, QHBoxLayout, QGroupBox, QSpinBox, QComboBox
+    QVBoxLayout, QHBoxLayout, QGroupBox, QSpinBox, QComboBox, QListWidget
 )
+from PyQt5.QtGui import QFont
 from CoordSelectionWidget import CoordSelectionWidget
 
 class MainWindow(QWidget):
@@ -20,7 +22,30 @@ class MainWindow(QWidget):
         self.setWindowTitle("Robot Controller")
 
         # Status Label
-        self.status = QLabel("Idle")
+        status_row = QHBoxLayout()
+        status_title = QLabel("Status:")
+        status_title.setStyleSheet("font-weight: bold;")
+        self.status_label = QLabel("Idle")
+        self.status_timestamp = datetime.now().strftime("%H:%M:%S")
+        self.status_label.setStyleSheet("font-weight: bold;")
+
+        status_row.addWidget(status_title)
+        status_row.addWidget(self.status_label)
+        status_row.addStretch()
+
+        # Status History Box
+        status_group = QGroupBox("History")
+        status_layout = QVBoxLayout()
+        self.status_history = QListWidget()
+        
+        status_history_font = QFont()
+        status_history_font.setPointSize(7)
+        self.status_history.setFont(status_history_font)
+        self.status_history.setFixedHeight(100)
+        status_layout.addWidget(self.status_history)
+
+        status_group.setLayout(status_layout)
+
 
         # -----------------------------
         # CONFIG PANEL
@@ -30,17 +55,19 @@ class MainWindow(QWidget):
         config_layout = QVBoxLayout()
 
         # Number of muscle pts
+        self.muscle_label = QLabel("Num Muscle Sampling Points:")
         self.num_muscle_pts = QSpinBox()
         self.num_muscle_pts.setRange(0, ImageWorker.max_num_muscle)
         self.num_muscle_pts.setValue(0)
-        config_layout.addWidget(QLabel("Num Muscle Sampling Points:"))
+        config_layout.addWidget(self.muscle_label)
         config_layout.addWidget(self.num_muscle_pts)
 
         # Number of marbling pts
+        self.marbling_label = QLabel("Num Marbling Sampling Points:")
         self.num_marbling_pts = QSpinBox()
         self.num_marbling_pts.setRange(0, ImageWorker.max_num_marbling)
         self.num_marbling_pts.setValue(1)
-        config_layout.addWidget(QLabel("Num Marbling Sampling Points:"))
+        config_layout.addWidget(self.marbling_label)
         config_layout.addWidget(self.num_marbling_pts)
 
         # Sampling Mode (Automatic/Manual)
@@ -50,6 +77,9 @@ class MainWindow(QWidget):
         config_layout.addWidget(self.sample_mode)
 
         config_box.setLayout(config_layout)
+
+        self.sample_mode.currentTextChanged.connect(self.onSampleModeUpdated)
+        self.onSampleModeUpdated(self.sample_mode.currentText())
 
 
         # -----------------------------
@@ -74,9 +104,10 @@ class MainWindow(QWidget):
         # -----------------------------
         # MAIN LAYOUT
         # -----------------------------
-        layout = QVBoxLayout()
-        layout.addWidget(self.status)
+        layout = QVBoxLayout()  
         layout.addWidget(config_box)
+        layout.addLayout(status_row)
+        layout.addWidget(status_group)
         layout.addLayout(btns)
         self.setLayout(layout)
 
@@ -90,11 +121,11 @@ class MainWindow(QWidget):
             return
         
         if hasattr(self, "robotWorker") and self.robotWorker.isRunning():
-            self.status.setText("Robot already running.")
+            self.update_status("Robot already running.")
             return
         
         if self.num_muscle_pts.value() + self.num_marbling_pts.value() == 0:
-            self.status.setText("Error: Total Number of sampling pts is 0")
+            self.update_status("Error: Total Number of sampling pts is 0")
             return
         
         # Create new RobotWorker each time Start is pressed
@@ -105,7 +136,7 @@ class MainWindow(QWidget):
 
         # Start the worker
         self.robotWorker.start()
-        # self.status.setText("Starting sample collection...")
+        # self.update_status("Starting sample collection...")
 
     def stop_robot(self):
         self.stop_ongoing = True
@@ -119,18 +150,18 @@ class MainWindow(QWidget):
                 
         self.stop_ongoing = False
     def reset_gui(self):
-        self.status.setText("Idle")
+        self.update_status("Idle")
 
     def on_progress(self, msg):
         # Check msg and handle properly
         if msg == "ROBOT_START":
-            self.status.setText("Starting sample collection...")
+            self.update_status("Starting sample collection...")
         elif msg == "ROBOT_STOP_SUCCESS":
-            self.status.setText("Sucessfully completed process.")
+            self.update_status("Sucessfully completed process.")
             time.sleep(2)
             # self.reset_gui()
         elif msg == "ROBOT_STOP":
-            self.status.setText("Stopped robot process.")
+            self.update_status("Stopped robot process.")
             time.sleep(2)
             # self.reset_gui()
         elif msg == "START_AUTOMATIC_POINT_SELECTION":
@@ -144,7 +175,7 @@ class MainWindow(QWidget):
             self.coord_selector.points_selected.connect(self.on_coord_result)
             self.coord_selector.show()
         else:
-            self.status.setText(msg)
+            self.update_status(msg)
         pass
 
     def on_coord_result(self, results):
@@ -154,6 +185,35 @@ class MainWindow(QWidget):
             self.image_coords_ready_event.set()
         elif self.stop_ongoing:
             self.stop_ongoing = False
+
+    def onSampleModeUpdated(self, mode):
+        is_auto = mode == "Automatic"
+
+        # self.muscle_label.setEnabled(is_auto)
+        self.num_muscle_pts.setEnabled(is_auto)
+        # self.marbling_label.setEnabled(is_auto)
+        self.num_marbling_pts.setEnabled(is_auto)
+
+    def update_status(self, message: str):
+        current = self.status_label.text()
+        if message != current:
+            # Get current timestamp
+            current = f"{self.status_timestamp} - {current}"
+
+            # Add previous state to history
+            self.status_history.insertItem(0, current)
+
+            # Update main current status label
+            self.status_label.setText(message)
+            self.status_timestamp = datetime.now().strftime("%H:%M:%S")
+
+            # Trim to last 10 entries
+            while self.status_history.count() > 10:
+                self.status_history.takeItem(self.status_history.count() - 1) #remove oldest
+
+            self.status_history.scrollToTop()
+        
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
